ee317a2449612695c96d68212b6efad6a4491627
Md. Nawrose <124398938+MdNawrose@users.noreply.github.com>
2024-12-10 21:48:06 +0600

    UC_Profile.Designer.cs

--- Pharmacy Management System 1.0/Pharmacy Management System 1.0/AdministratorUC/UC_Profile.Designer.cs
+++ Pharmacy Management System 1.0/Pharmacy Management System 1.0/AdministratorUC/UC_Profile.Designer.cs
@@ -3,7 +3,7 @@
     partial class UC_Profile
     {
        
-        /// Required designer variable.
+        
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
